b81276de59ae65eb257f212ea51591670d513051
shoaib mehmood <iamsmabbasi@gmail.com>
2023-08-22 09:47:03 +0500

    category

---
+++ Online_Shopping_Cart/Controllers/CategoryController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Online_Shopping_Cart.Models;
+
+namespace Online_Shopping_Cart.Controllers
+{
+    public class CategoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Create()
+        {
+            return View(); 
+        }
+        [HttpPost]
+        public IActionResult Create(Category model )
+        {
+            if(ModelState.IsValid)
+            {
+                var x = 01;
+            }
+            // ModelState.AddModelError("Name", "Custom error");
+            return View(model) ;
+        }
+        public IActionResult Edit()
+        {
+            return View();
+        } 
+        public IActionResult Delete()
+        {
+            return View();
+        }
+
+    }
+}
